e666781488cbbfe44ce654e9091d96c048df76ee
DDXY1230 <2502752288@qq.com>
2023-03-12 13:16:14 +0800

    first commit

---
+++ README.md
@@ -0,0 +1,5 @@
+# Vue 3 + Vite
+移动端
+1. vant组件库
+2. 安装vant组件库`npm i vant` 
+  同时也推荐一个插件`npm i unplugin-vue-components -D`
---
+++ src/App.vue
@@ -0,0 +1,25 @@
+<!--  -->
+<script setup>
+import {ref,reactive} from 'vue'
+import cookbook from '@/assets/images/cookbook.png'
+import cookbookActive from '@/assets/images/cookbook-active.png'
+const active = ref(0);
+</script>
+<template>
+<div class=''>
+  <van-tabbar v-model="active">
+  <van-tabbar-item icon="search">
+    <span>菜谱大全</span>
+    <template #icon="props">
+      <img :src="props.active ? cookbookActive : cookbook" alt="">
+    </template>
+  </van-tabbar-item>
+  <van-tabbar-item icon="friends-o">标签</van-tabbar-item>
+  <van-tabbar-item icon="setting-o">标签</van-tabbar-item>
+</van-tabbar>
+</div>
+</template>
+
+<style lang='scss' scoped>
+
+</style>
